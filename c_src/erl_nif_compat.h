3b5f7eaa74948a0826321a1147effdb610b25f1d
Ilya Dmitrichenko <ilya.dmitrichenko@pachube.com>
2014-06-30 14:46:03 +0000

    Make it compile with Erlang R17

--- c_src/erl_nif_compat.h
+++ c_src/erl_nif_compat.h
@@ -5,22 +5,22 @@
 extern "C" {
 #endif /* __cplusplus */
 
-#include "erl_nif.h"
+#include <erl_nif.h>
 
-#if ERL_NIF_MAJOR_VERSION == 1 && ERL_NIF_MINOR_VERSION == 0
+//#if ERL_NIF_MAJOR_VERSION == 1 && ERL_NIF_MINOR_VERSION == 0
+//
+//#define enif_open_resource_type_compat enif_open_resource_type
+//#define enif_alloc_resource_compat enif_alloc_resource
+//#define enif_release_resource_compat enif_release_resource
+//#define enif_alloc_binary_compat enif_alloc_binary
+//#define enif_realloc_binary_compat enif_realloc_binary
+//#define enif_release_binary_compat enif_release_binary
+//#define enif_alloc_compat enif_alloc
+//#define enif_free_compat enif_free
+//
+//#endif /* R13B04 */
 
-#define enif_open_resource_type_compat enif_open_resource_type
-#define enif_alloc_resource_compat enif_alloc_resource
-#define enif_release_resource_compat enif_release_resource
-#define enif_alloc_binary_compat enif_alloc_binary
-#define enif_realloc_binary_compat enif_realloc_binary
-#define enif_release_binary_compat enif_release_binary
-#define enif_alloc_compat enif_alloc
-#define enif_free_compat enif_free
-
-#endif /* R13B04 */
-
-#if ERL_NIF_MAJOR_VERSION == 2 && ERL_NIF_MINOR_VERSION == 0
+//#if ERL_NIF_MAJOR_VERSION == 2 && ERL_NIF_MINOR_VERSION == 0
 
 #define enif_open_resource_type_compat(E, N, D, F, T) \
     enif_open_resource_type(E, NULL, N, D, F, T)
@@ -46,7 +46,7 @@ extern "C" {
 #define enif_free_compat(E, P) \
     enif_free(P)
 
-#endif /* R14 */
+//#endif /* R14 */
 
 
 
